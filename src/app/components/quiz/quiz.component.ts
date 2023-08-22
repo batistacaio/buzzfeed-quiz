8144958d07a633425f4592a47e5174bba588e6b4
Caio Batista <caio_b_s@hotmail.com>
2023-08-22 13:43:06 -0300

    feat: created quiz component style and logic

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quiz-questions.json'
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  title: string = ''
 
+  questions: any
+  questionSelected: any
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  answers: string[] = []
+  answerSelected: string = ''
+
+  finished: boolean = false
+
+  ngOnInit(): void {
+    if(quiz_questions){
+      this.finished = false
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  submitAnswer(value: string) {
+    this.answers.push(value)
+    this.nextQuestion()
+  }
+
+  nextQuestion() {
+    this.questionIndex++
+
+    if(this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+  }
 }
